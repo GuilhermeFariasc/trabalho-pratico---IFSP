5dadbb2bfcadded8fed6f0ecee94c5e2dd2d4315
Guilherme Farias <guilhermefarias.cs@gmail.com>
2020-09-14 14:48:31 -0300

    init

---
+++ tp_01/tp_01/tp_01/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using tp_01.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+
+namespace tp_01
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new NavigationPage(new Page1());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ tp_01/tp_01/tp_01/Pages/Page1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tp_01.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtOk_ClickedAsync(object sender, EventArgs e)
+        {
+            string id = etId.Text;
+            string pasw = etPasw.Text;
+
+            if (id != "admin" || pasw != "@dmin")
+            {
+                await DisplayAlert("Erro", "Login invalido, tente novamente!", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("Sucesso", "Login Realizado!", "Ok");
+            }
+
+        }
+        void OnClickLimpar(object limpar, EventArgs args)
+        {
+            this.etId.Text = "";
+            this.etPasw.Text = "";
+        }
+
+        async void OnClickCredito(object credito, EventArgs args)
+        {
+            await DisplayAlert("Creditos", "Guilherme Farias\r\nIgor Luiz", "OK");
+        }
+    }
+}
